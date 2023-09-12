628594b455e4ceabb71fcf85320028eabe1b1dbb
yassinebenaid <yassinebenaide3@gmail.com>
2023-09-12 20:47:41 +0100

    added our first parsing step, parser for variable binding which makes the tests pass

--- parser/parser.go
+++ parser/parser.go
@@ -28,5 +28,64 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	prog := &ast.Program{}
+	prog.Statements = make([]ast.Statement, 0)
+
+	for p.currentToken.Type != token.EOF {
+		stat := p.parseStatement()
+
+		if stat != nil {
+			prog.Statements = append(prog.Statements, stat)
+		}
+
+		p.nextToken()
+	}
+
+	return prog
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currentToken.Type {
+	case token.VAR:
+		return p.parseVarStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseVarStatement() ast.Statement {
+	stat := &ast.VarStatement{Token: p.currentToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stat.Name = &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.currentTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stat
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+
+	return false
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return t == p.peekToken.Type
+}
+
+func (p *Parser) currentTokenIs(t token.TokenType) bool {
+	return t == p.currentToken.Type
 }
