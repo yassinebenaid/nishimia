9c0aab9c10cabfae19b1a68c012c317cd103bbb2
yassinebenaid <yassinebenaide3@gmail.com>
2023-09-18 20:54:16 +0100

    quick fix

--- main.go
+++ main.go
@@ -5,10 +5,28 @@ import (
 	"os"
 	"os/user"
 
+	"github.com/yassinebenaid/nishimia/eval"
+	"github.com/yassinebenaid/nishimia/lexer"
+	"github.com/yassinebenaid/nishimia/object"
+	"github.com/yassinebenaid/nishimia/parser"
 	"github.com/yassinebenaid/nishimia/repl"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		Interactive()
+		os.Exit(0)
+	}
+
+	if len(os.Args) == 2 {
+		RunFile(os.Args[1])
+		os.Exit(0)
+	}
+
+	fmt.Println("undefined options", os.Args)
+}
+
+func Interactive() {
 	user, err := user.Current()
 
 	if err != nil {
@@ -20,4 +38,21 @@ func main() {
 	repl.Start(os.Stdin, os.Stdout)
 
 	fmt.Print("\nGood by !\n")
+}
+
+func RunFile(fn string) {
+	file, err := os.ReadFile(fn)
+	if err != nil {
+		fmt.Println("Error: \n\t", err)
+	}
+
+	env := object.NewEnvirement()
+	lex := lexer.New(string(file))
+	par := parser.New(lex)
+	program := par.ParseProgram()
+	result := eval.Eval(program, env)
+
+	if result.Type() != object.NULL_OBJ {
+		fmt.Println(result.Inspect())
+	}
 }
