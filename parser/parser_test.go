549cdeb3fe6a21f945c8d16a35900f2b608f67b8
yassinebenaid <yassinebenaide3@gmail.com>
2023-09-22 16:38:40 +0100

    added new test suite

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -355,6 +355,8 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 
 		{"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))", "add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))"},
 		{"add(a + b + c * d / f + g)", "add((((a + b) + ((c * d) / f)) + g))"},
+
+		{"add(a[0] + b[2] + c[3] * d[4] / f[5] + g[6])", "add((((a[0] + b[2]) + ((c[3] * d[4]) / f[5])) + g[6]))"},
 	}
 
 	for _, tt := range tests {
